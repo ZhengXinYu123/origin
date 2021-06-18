5e575285480f7ce4a2f50f5d08cc85d2e08dadd3
ZhengXinYu <1340433102@qq.com>
2021-06-18 16:38:49 +0800

    second commit

--- IdeaProjects/untitled13/src/main/java/com/git/gittest.java
+++ IdeaProjects/untitled13/src/main/java/com/git/gittest.java
@@ -3,6 +3,7 @@ package com.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hlleo git!");
+        System.out.println("hlleo git2!");
 
     }
 }
